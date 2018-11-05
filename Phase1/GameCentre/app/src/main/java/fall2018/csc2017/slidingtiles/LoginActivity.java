ed90aa3786c262737bb519ce6eeaeda28d12a93f
Magomed Evloev <magomed.evloev@mail.utoronto.ca>
2018-11-05 14:32:28 -0500

    LoginActivity checks if user exists and if password matches

--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/LoginActivity.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/LoginActivity.java
@@ -7,6 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
     static String username;
@@ -14,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     public static final String FILENAME = "/data/data/fall2018.csc2017.slidingtiles/files/AccountActivity.ser";
     EditText usernameInput;
     EditText passwordInput;
+    Button SignIn;
+    Map<String, UserAccount> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +38,36 @@ public class LoginActivity extends AppCompatActivity {
      * Activate the ChooseGameActivity button.
      */
     private void addSignInButtonListener() {
-        Button SignIn = findViewById(R.id.SignIn);
+        SignIn = findViewById(R.id.SignIn);
         SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switchToChooseGameActivity();
-            }
-        });
-    }
+                usernameInput = (EditText) findViewById(R.id.usernameText);
+                passwordInput = (EditText) findViewById(R.id.passwordText);
+                username = usernameInput.getText().toString();
+                password = usernameInput.getText().toString();
+                UserAccount newUser = new UserAccount(username, password);
+                try {
+                    // read object from file
+                    FileInputStream fis = new FileInputStream(FILENAME);
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+                    result = (Map<String, UserAccount>) ois.readObject();
+                    ois.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                if(result.containsKey(username) && result.get(username).checkPassword(password)) {
+                    switchToChooseGameActivity();
+                }
 
+            }
+        }
+        );
+    }
     private void switchToChooseGameActivity() {
         Intent tmp = new Intent(this, ChooseGameActivity.class);
         startActivity(tmp);
